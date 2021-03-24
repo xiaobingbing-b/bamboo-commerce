36a69e23af897e640e4c0c169f26ae2f686011dc
1198087323@qq.com <tangbing1301>
2021-03-24 23:53:10 +0800

    123321

---
+++ commerce-gateway/src/main/java/com/bamboo/commerce/gateway/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.bamboo.commerce.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        Map<String, CorsConfiguration> corsConfigurations = new HashMap<>();
+        //设置相关属性
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
+        corsConfiguration.applyPermitDefaultValues();
+        corsConfigurations.put("/**", corsConfiguration);
+
+        corsConfigurationSource.setCorsConfigurations(corsConfigurations);
+        return new CorsWebFilter(corsConfigurationSource);
+    }
+}
